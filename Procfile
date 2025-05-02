30dafb1346d653fad91b86f2f3017a5322c96526
sona-poddar <sonapoddar300703@gmail.com>
2025-05-02 17:11:08 +0530

    Final deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn stocksim.wsgi:application
--- stocksim/settings.py
+++ stocksim/settings.py
@@ -85,6 +85,8 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
+             conn_max_age=600,
+             ssl_require=True
         }
     }
 
